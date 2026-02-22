bad6f8bb71d94ea3c5f27548ac54ac16d8507005
palojuniorkamel <palojuniorkamel@gmail.com>
2026-02-22 16:21:14 +0000

    feat: Update dependencies and resolve merge conflict

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -11,6 +11,7 @@ import 'package:geolocator_web/geolocator_web.dart';
 import 'package:maplibre_gl_web/maplibre_gl_web.dart';
 import 'package:package_info_plus/src/package_info_plus_web.dart';
 import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:sentry_flutter/sentry_flutter_web.dart';
 import 'package:wakelock_plus/src/wakelock_plus_web_plugin.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
@@ -21,6 +22,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   MapLibreMapPlugin.registerWith(registrar);
   PackageInfoPlusWebPlugin.registerWith(registrar);
   WebPermissionHandler.registerWith(registrar);
+  SentryFlutterWeb.registerWith(registrar);
   WakelockPlusWebPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
